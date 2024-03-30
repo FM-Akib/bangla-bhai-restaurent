4c00356fe620834487d49782ca162f28bb20958d
FM-Akib <muntasirakib08@gmail.com>
2024-03-31 00:27:36 +0600

    Footer added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            This is the Home
+           <Banner></Banner>
         </div>
     );
 };
