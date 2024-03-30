e5740c49ed6ce371de263c57a8bd28b8db17640b
FM-Akib <muntasirakib08@gmail.com>
2024-03-31 03:38:33 +0600

    Banner section added

--- src/Pages/Shared/Nav/Navbar.jsx
+++ src/Pages/Shared/Nav/Navbar.jsx
@@ -2,7 +2,7 @@
 
 const Navbar = () => {
     return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-black text-white fixed z-10 bg-opacity-30 max-w-screen-xl">
      <div className="navbar-start">
       <div className="dropdown">
        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
