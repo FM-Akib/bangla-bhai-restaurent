822d9016888e9d33d1de866356223532bf95aff0
FM-Akib <muntasirakib08@gmail.com>
2024-03-30 23:55:42 +0600

    Nav and route updated

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            This is the Home
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            Fooooooooooooooooooooooot
+        </div>
+    );
+};
+
+export default Footer;
