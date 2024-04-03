0684c3b2c29c3504424a2d5a3ada5ada18d61b77
FM-Akib <muntasirakib08@gmail.com>
2024-04-03 23:44:43 +0600

    added order cover

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,8 +1,17 @@
+import { Helmet } from "react-helmet";
+import Cover from "../../Shared/Cover/Cover";
+import orderimg from "../../../assets/shop/banner2.jpg";
 
 const Order = () => {
     return (
         <div>
-            
+             <Helmet>
+            <title>Bangla Bhai | Order</title>
+            </Helmet>
+
+            <Cover img={orderimg} title='OUR SHOP' subtitle='Would you like to try a dish?'></Cover>
+
+
         </div>
     );
 };
