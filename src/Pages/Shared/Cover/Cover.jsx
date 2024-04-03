0f297469375660b4d963d21d688ec2c94e601a9f
FM-Akib <muntasirakib08@gmail.com>
2024-04-03 10:03:06 +0600

    added parallax

--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,8 +1,15 @@
+import { Parallax } from 'react-parallax';
 
 const Cover = ({img,title,subtitle}) => {
     return (
-        <div className="hero h-[700px]" style={{backgroundImage: `url(${img})`}}>
-            <div className="hero-overlay bg-opacity-60"></div>
+        <Parallax
+        blur={{ min: -80, max: 80 }}
+        bgImage={img}
+        bgImageAlt="cover"
+        strength={-200}
+    >
+        <div className="hero h-[700px]">
+          <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md text-white">
                 <h1 className="mb-5 text-6xl font-bold uppercase">{title}</h1>
@@ -10,7 +17,9 @@ const Cover = ({img,title,subtitle}) => {
                 
                 </div>
             </div>
-    </div>
+        </div>
+    </Parallax>
+
     );
 };
 
