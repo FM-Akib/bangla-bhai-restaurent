dd74aab3ed658b24309553fe9fb37e4eb11d5ac6
FM-Akib <muntasirakib08@gmail.com>
2024-04-04 00:47:15 +0600

    added order cover

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -2,7 +2,12 @@ import { Helmet } from "react-helmet";
 import Cover from "../../Shared/Cover/Cover";
 import orderimg from "../../../assets/shop/banner2.jpg";
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { useState } from "react";
+
 const Order = () => {
+    const [tabIndex,setTabindex] = useState(0);
     return (
         <div>
              <Helmet>
@@ -10,6 +15,23 @@ const Order = () => {
             </Helmet>
 
             <Cover img={orderimg} title='OUR SHOP' subtitle='Would you like to try a dish?'></Cover>
+
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabindex(index)}>
+           <div className="my-10 flex justify-center uppercase font-semibold">
+           <TabList >
+                <Tab>Salad</Tab>
+                <Tab>pizza</Tab>
+                <Tab>soups</Tab>
+                <Tab>desserts</Tab>
+                <Tab>drinks</Tab>
+            </TabList>
+           </div>
+            <TabPanel>a</TabPanel>
+            <TabPanel>b</TabPanel>
+            <TabPanel>c</TabPanel>
+            <TabPanel>d</TabPanel>
+            <TabPanel>e</TabPanel>
+            </Tabs>
 
 
         </div>
