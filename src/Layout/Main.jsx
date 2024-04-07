d88d0cb1ac73e7bee347d0cb8b2be44792fd2c53
FM-Akib <muntasirakib08@gmail.com>
2024-04-08 01:42:45 +0600

    added login sign up

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,useLocation } from "react-router-dom";
 import Navbar from "../Pages/Shared/Nav/Navbar";
 import Footer from "../Pages/Shared/Footer/Footer";
 import '../App.css';
 
 const Main = () => {
+    const location = useLocation();
+    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
+
     return (
         <div className="Main">
-           <Navbar></Navbar>
-           <Outlet></Outlet>
-           <Footer></Footer>
+            {noHeaderFooter || <Navbar></Navbar>}
+            <Outlet></Outlet>
+            {noHeaderFooter || <Footer></Footer>}
         </div>
     );
 };
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            jdfffffffffffffffff
+        </div>
+    );
+};
+
+export default Login;
