e8a74838dbec317dc9b23c0c18330136ebf7a96a
FM-Akib <muntasirakib08@gmail.com>
2024-04-09 02:48:15 +0600

    set private route

--- src/Pages/Shared/Secrect/Secrect.jsx
+++ src/Pages/Shared/Secrect/Secrect.jsx
@@ -2,7 +2,7 @@
 
 const Secrect = () => {
     return (
-        <div>
+        <div className="pt-20">
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ipsum veritatis quae eius, quaerat ipsam sint neque, nulla quos fuga maxime, illum qui quis animi earum temporibus. Quam ea, quaerat ad ipsum aliquam necessitatibus ullam harum ipsam similique deserunt doloribus odio aliquid eveniet officia atque, nulla consectetur saepe vitae id.</p>           
         </div>
     );
