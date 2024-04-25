9ad1cf48345277a2b18bdb6bcf51799ce0e6e681
FM-Akib <muntasirakib08@gmail.com>
2024-04-26 01:27:37 +0600

    axiosSecure with JWT

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,41 @@
 import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 
 const axioSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
  const useAxiosSecure = () => {
+    const navigate = useNavigate()
+    const {logOut} = useContext(AuthContext)
+
+
+
+    axioSecure.interceptors.request.use(function (config) {
+        // Do something before request is sent
+        const token = localStorage.getItem('access-token')
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+      }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
+
+      // Add a response interceptor
+      axioSecure.interceptors.response.use(function (response) {
+      return response;
+     },  async(error)=> {
+     const status = error.response.status;
+    
+     if(status === 401 || status === 403){
+        await logOut();
+        navigate('/login')
+     }
+     return Promise.reject(error);
+  });
+
     return axioSecure;
 };
 
--- src/Pages/Dashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers.jsx
@@ -9,11 +9,7 @@ const AllUsers = () => {
     const { isPending, error, data:users=[],refetch } = useQuery({
         queryKey: ['allUsers'],
         queryFn: async ()=>{
-            const res = await axiosSecure.get('/users',{
-              headers: { 
-                authorization: `Bearer ${localStorage.getItem('access-token')}`
-              }
-            })
+            const res = await axiosSecure.get('/users')
             return res.data;
         }
       })
