7e90604e80c1c6390faaa8008a219b2be4c996ef
FM-Akib <muntasirakib08@gmail.com>
2024-04-30 02:17:35 +0600

    added add item page

--- src/Pages/Dashboard/AddItems.jsx
+++ src/Pages/Dashboard/AddItems.jsx
@@ -1,15 +1,51 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../Shared/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa6";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+// import Category from "../Home/Category/Category";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+const image_hosting_key= import.meta.env.VITE_Image_Hosting_key;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 
 const AddItems = () => {
     const { register, handleSubmit } = useForm()
-    const onSubmit = (data) => console.log(data)
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+    const onSubmit = async (data) => {
+        const imageFile = {image: data.image[0]}
+        const res = await axiosPublic.post(image_hosting_api,imageFile,{
+            headers: {
+                'content-type': 'multipart/form-data',
+            }
+        }) 
+        if(res.data.success) {
+            const menuItem ={
+                name: data.name,
+                category:data.category,
+                price: parseFloat(data.price),
+                recipe: data.recipe,
+                image: res.data.data.display_url
+            }
+            const menuRes = await axiosSecure.post('/menu',menuItem)
+            if(menuRes.data.insertedId){
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${data.name} added!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+
+        }
+    }
     return (
         <div>
             <SectionTitle heading="Add an Item" subHeading="What's new?"></SectionTitle>
 
-            <div className="bg-slate-100 p-5 rounded-md">
+            <div className="bg-slate-100 p-10 rounded-md">
 
             <form onSubmit={handleSubmit(onSubmit)}>
 
@@ -23,7 +59,7 @@ const AddItems = () => {
 
             <div className="flex gap-6 items-end my-2">
 
-            <select {...register("category")} className="select select-bordered w-full ">
+            <select {...register("category")} defaultValue="default" className="select select-bordered w-full ">
             <option disabled selected>Category*</option>
             <option value="salad">Salad</option>
             <option value="pizza">Pizza</option>
