d2103896c1c2037f31f6029e4759e9af8c303ab8
FM-Akib <muntasirakib08@gmail.com>
2024-05-04 21:44:11 +0600

    updated admin stats

--- src/Pages/Dashboard/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome.jsx
@@ -5,6 +5,21 @@ import { AuthContext } from "../../Provider/AuthProvider";
 import { ImUsers } from "react-icons/im";
 import { RiFileList2Fill } from "react-icons/ri";
 import { BiSolidBadgeDollar } from "react-icons/bi";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+  
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
 
 const AdminHome = () => {
     const axiosSecure = useAxiosSecure();
@@ -17,7 +32,16 @@ const AdminHome = () => {
             return res.data;
         }
     })
-    console.log(stats)
+    // console.log(stats)
+
+    const {data:chartData=[]} = useQuery({
+        queryKey:['order-stats'],
+        queryFn: async()=>{
+            const res = await axiosSecure.get('/order-stats')
+            return res.data;
+        }
+    })
+    console.log(chartData)
     return (
         <div>
             <div className="">
@@ -28,10 +52,6 @@ const AdminHome = () => {
                 }
                 </h1>
             </div>
-
-
-
-
 
     <div className="">
            <section className="p-4 my-6 bg-gray-100 text-gray-800 rounded-md">
@@ -78,6 +98,36 @@ const AdminHome = () => {
 		</div>
 	</div>
 </section>
+           </div>
+
+
+{/* Charts Section */}
+           <div className="grid-cols-2">
+              
+              <div className="col-span-1">
+              <BarChart
+      width={500}
+      height={300}
+      data={chartData}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="category" />
+      <YAxis />
+      <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {chartData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+        ))}
+      </Bar>
+    </BarChart>
+              </div>
+
+              <div className="col-span-1"></div>
            </div>
             
         </div>
